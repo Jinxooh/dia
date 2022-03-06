7687f6d563392d6da3f9ab645abfabfce9e6bf8d
Jeckson <cjswp122@gmail.com>
2022-03-07 00:54:00 +0900

    추가사항 반영

--- src/pages/Statistic.js
+++ src/pages/Statistic.js
@@ -1,4 +1,4 @@
-import { LeftMenu, MainContainer } from "../components";
+import { ContentContainer, LeftMenu, MainContainer } from "../components";
 import Data from '../assets/data/data.json'
 
 const Statistic = () => {
@@ -6,10 +6,10 @@ const Statistic = () => {
   return (
     <MainContainer>
       <LeftMenu data={statistic}></LeftMenu>
-      <div>
+      <ContentContainer>
         <h1>소개</h1>
         <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
-      </div>
+      </ContentContainer>
     </MainContainer>
   );
 };
